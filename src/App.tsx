2c255f6f7921e8ff4eefcc966306b8dcdc9eafcd
humiba <huaminhbao7a4@gmail.com>
2023-11-24 07:40:49 +0700

    setup project code base

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+
+import "./globals.css";
+import { SignInForm, SignUpForm } from "./_auth/forms";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+
+const App = () => {
+  return (
+    <main className="h-screen flex ">
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
